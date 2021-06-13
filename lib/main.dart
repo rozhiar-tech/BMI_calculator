b2f711890da8d2a353e575056b6ebb06cc26ccc3
Rozhiar <rozyar.313175000@uhd.edu.iq>
2021-06-13 17:35:38 +0300

    added cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:BMIcalculator/Screens/input_page.dart';
 
+import 'Screens/profile.dart';
+
 void main() {
   runApp(BmiCalculator());
 }
@@ -9,7 +11,8 @@ class BmiCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
+      home: Profile(),
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xff0a0e21),
         scaffoldBackgroundColor: Color(0xff0a0e21),
