6c8290e7c1fc9d809a2c1812bbbcc9192075c926
Rozhiar <rozyar.313175000@uhd.edu.iq>
2021-06-16 00:20:00 +0300

    Adding Features

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -1,11 +1,22 @@
 import 'package:BMIcalculator/model/meal.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'input_page.dart';
+import 'package:vector_math/vector_math_64.dart' as math;
 
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    var hour = DateTime.now().hour;
+    var minute = DateTime.now().minute;
+    var seconds = DateTime.now().second;
+
+    var berlinWallFell = DateTime.utc(1989, 11, 9);
+    var moonLanding = DateTime.parse("1969-07-20 20:18:04Z");
+
     return Scaffold(
       backgroundColor: const Color(0xFFe9e9e9),
       bottomNavigationBar: ClipRRect(
@@ -65,6 +76,145 @@ class Profile extends StatelessWidget {
               ),
               child: Container(
                 color: Colors.white,
+                padding: EdgeInsets.only(
+                  top: 50,
+                  left: 32,
+                  right: 16,
+                  bottom: 10,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    ListTile(
+                      title: Text(
+                        "$hour :$minute :$seconds",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "چۆنی، ڕۆژیار",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                      trailing: GestureDetector(
+                        child: ClipOval(
+                          child: Image.asset("assets/User.png"),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => InputPage()),
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            GestureDetector(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage('assets/trainer.png'),
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 5,
+                                      blurRadius: 7,
+                                      offset: Offset(
+                                          0, 3), // changes position of shadow
+                                    ),
+                                  ],
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                height: height * 0.13,
+                                width: width * 0.27,
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => InputPage()),
+                                );
+                              },
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'قسە لەگەڵ ڕاهێنەر بکە',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Protein',
+                              style: TextStyle(color: Colors.black54),
+                            ),
+                            _IngredientProgress(
+                              ingredient: "Protein",
+                              progress: 0.3,
+                              progressColor: Colors.green,
+                              leftAmount: 72,
+                              width: width * 0.28,
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'water',
+                              style: TextStyle(color: Colors.black54),
+                            ),
+                            _IngredientProgress(
+                              ingredient: "Carbs",
+                              progress: 0.7,
+                              progressColor: Colors.red,
+                              leftAmount: 252,
+                              width: width * 0.28,
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              'Protein',
+                              style: TextStyle(color: Colors.black54),
+                            ),
+                            _IngredientProgress(
+                              ingredient: "Fat",
+                              progress: 0.1,
+                              progressColor: Colors.yellow,
+                              leftAmount: 300,
+                              width: width * 0.28,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -73,7 +223,7 @@ class Profile extends StatelessWidget {
             left: 0,
             right: 0,
             child: Container(
-              height: height * 0.5,
+              height: height * 0.52,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -232,6 +382,144 @@ class Profile extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class _IngredientProgress extends StatelessWidget {
+  final String ingredient;
+  final int leftAmount;
+  final double progress, width;
+  final Color progressColor;
+
+  const _IngredientProgress(
+      {Key key,
+      this.ingredient,
+      this.leftAmount,
+      this.progress,
+      this.progressColor,
+      this.width})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          ingredient.toUpperCase(),
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: 10,
+                  width: width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    color: Colors.black12,
+                  ),
+                ),
+                Container(
+                  height: 10,
+                  width: width * progress,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    color: progressColor,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              width: 5,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _RadialProgress extends StatelessWidget {
+  final double height, width, progress;
+
+  const _RadialProgress({Key key, this.height, this.width, this.progress})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _RadialPainter(
+        progress: 0.7,
+      ),
+      child: Container(
+        height: height,
+        width: width,
+        child: Center(
+          child: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "1731",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                    color: const Color(0xFF200087),
+                  ),
+                ),
+                TextSpan(text: "\n"),
+                TextSpan(
+                  text: "kcal left",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: const Color(0xFF200087),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RadialPainter extends CustomPainter {
+  final double progress;
+
+  _RadialPainter({this.progress});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..strokeWidth = 10
+      ..color = Color(0xFF200087)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double relativeProgress = 360 * progress;
+
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: size.width / 2),
+      math.radians(-90),
+      math.radians(-relativeProgress),
+      false,
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
 
